653a416e6d597aa5922793705c7ac81095d637ad
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-01-12 23:00:32 +0630

    feat(toolkit): download static file feat and example added

---
+++ app-download/go.mod
@@ -0,0 +1,3 @@
+module github.com/kaungmyathan22/golang-toolkit/app-download
+
+go 1.21.5
--- toolkit/tools.go
+++ toolkit/tools.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path"
 	"path/filepath"
 	"regexp"
 	"strings"
@@ -154,4 +155,11 @@ func (t *Tools) Slugify(s string) (string, error) {
 		return "", errors.New("after removing characters, slug is zero length")
 	}
 	return slug, nil
+}
+
+func (t *Tools) DownloadStaticFile(w http.ResponseWriter, r *http.Request, p, file, displayName string) {
+	fp := path.Join(p, file)
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", displayName))
+
+	http.ServeFile(w, r, fp)
 }
